24486f341726e161164ed0deea2c27220e06ee1e
Buddynice34 <willemapril33@gmail.com>
2026-01-20 10:17:15 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="px-4 py-3 mt-4 ml-6 bg-red-500 hover:bg-blue-500 transition duration-300 rounded-lg w-fit text-white font-semibold">
+      Hello World
+    </div>
+  )
+}
+
+export default App
