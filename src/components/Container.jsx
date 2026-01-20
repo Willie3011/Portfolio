15494ef60c6e536f8a935a26e6bc47a937f04510
Buddynice34 <willemapril33@gmail.com>
2026-01-20 19:35:42 +0200

    Add Container component and update Homepage layout
    Introduced a reusable Container component for consistent page layout. Updated Homepage to use the new Container and applied custom fonts and color variables in index.css for improved styling.

---
+++ src/components/Container.jsx
@@ -0,0 +1,8 @@
+
+function Container({children}) {
+  return (
+      <section className="w-full max-w-7xl mx-auto my-0 py-0 px-8">{children}</section>
+  )
+}
+
+export default Container
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Container from '../components/Container'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className='bg-secondary h-screen font-Poppins'>
+      <Container>Hello world</Container>
+    </div>
   )
 }
 
