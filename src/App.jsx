df933ad27dfc0126f7e097e97cb2e5359a2f3c22
Buddynice34 <willemapril33@gmail.com>
2026-01-20 10:34:43 +0200

    Add react-query, axios, and homepage route setup
    Installed @tanstack/react-query and axios dependencies. Refactored App.jsx to use React Router and added a Homepage component at the root route. Wrapped the app in QueryClientProvider in main.jsx to enable react-query throughout the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
 function App() {
-
   return (
-    <div className="px-4 py-3 mt-4 ml-6 bg-red-500 hover:bg-blue-500 transition duration-300 rounded-lg w-fit text-white font-semibold">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
