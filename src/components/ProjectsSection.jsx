9bda5c8f04aaa4dc98eb08a0788f7bd5faf56084
Buddynice34 <willemapril33@gmail.com>
2026-02-07 17:04:45 +0200

    Updated ProjectList
    - Updated ProjectList to receive projects as props
    - ProjectsSection and ProjectsPage now fetch different types of project.
    -ProjectsSection fetches featured projects
    - ProjectsPage fetches all projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from './Container'
 import SectionHeading from './SectionHeading'
 import ProjectList from './ProjectList'
+import axios from 'axios'
+
+const fetchProjects = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/projects/featured`)
+
+  return response.data.projects.data;
+}
 
 function ProjectsSection() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetchProjects();
+
+      if (response) {
+        setProjects(response);
+      }
+    }
+
+    getData();
+  }, [])
   return (
       <div className='my-16 py-16'>
           <Container>
               <SectionHeading heading="Projects" paragraph="Here you will find some of the personal projects that I created."/>
-              <ProjectList/>
+        <ProjectList projects={projects} />
           </Container>
     </div>
   )
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,34 @@
+import axios from 'axios';
 import Container from '../components/Container';
 import ProjectList from '../components/ProjectList';
 import SectionHeading from "../components/SectionHeading";
+import { useEffect, useState } from 'react';
+
+const fetchProjects = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/projects`)
+
+  return response.data.projects.data;
+}
+
 function ProjectsPage() {
+    const [projects, setProjects] = useState([]);
+  
+    useEffect(() => {
+      const getData = async () => {
+        const response = await fetchProjects();
+  
+        if (response) {
+          setProjects(response);
+        }
+      }
+  
+      getData();
+    }, [])
   return (
     <div className='py-8'>
       <Container>
         <SectionHeading heading="My Projects"/>
-        <ProjectList />
+        <ProjectList projects={projects}/>
       </Container>
     </div>
   )
