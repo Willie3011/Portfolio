03b794549e429aed262313090d3bb13b64bc97ed
Buddynice34 <willemapril33@gmail.com>
2026-07-10 17:55:02 +0200

    fixed bug: spacing issue
    Fixed the spacing issue on the banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <nav className='bg-black text-white h-16 border-b border-white/15 relative'>
-            <div className="container flex items-center justify-between">
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-black text-white h-16 border-b border-white/15'>
+            <div className="container flex items-center justify-between relative">
                 {/* left */}
                 <div className="font-Barlow font-bold text-2xl">WA<em className='text-2xl text-accent'>.</em></div>
                 {/* right */}
-                <ul className='flex items-center gap-8'>
+                <ul className='hidden items-center gap-8 md:flex'>
                     <li><Link className='nav-link' to='/'>Home</Link></li>
                     <li><Link className='nav-link' to='/about'>About</Link></li>
                     <li><Link className='nav-link' to='/work'>My Work</Link></li>
